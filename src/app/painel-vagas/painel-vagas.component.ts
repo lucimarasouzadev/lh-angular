4891429d99e4ea94578ec908a90309f1226cdd20
Lucimara Souza <lucimara.souza.dev@gmail.com>
2022-02-09 16:39:01 -0300

    Subindo LH Angular

---
+++ src/app/painel-vagas/painel-vagas.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { Vaga } from '../models/Vagas.model';
+import { VagasService } from '../vagas.service';
+
+@Component({
+  selector: 'app-painel-vagas',
+  templateUrl: './painel-vagas.component.html',
+  styleUrls: ['./painel-vagas.component.css'],
+})
+export class PainelVagasComponent implements OnInit {
+  public vaga: Vaga = new Vaga(0, '', '', '', 0);
+
+  constructor(private _vagasService: VagasService) {}
+
+  ngOnInit(): void {}
+
+  cadastrar() {
+    this._vagasService.cadastrarVaga(this.vaga).subscribe(
+      (vaga) => {
+        this.vaga = new Vaga(0, '', '', '', 0);
+      },
+      (err) => {
+        console.log('erro ao cadastrar');
+      }
+    );
+    window.location.href = '/mural-vagas';
+  }
+
+  atualizar(id: number) {
+    this._vagasService.atualizarVaga(id, this.vaga).subscribe(
+      (vaga) => {
+        this.vaga = new Vaga(0, '', '', '', 0);
+      },
+      (err) => {
+        console.log('erro ao atualizar');
+      }
+    );
+    window.location.href = '/mural-vagas';
+  }
+
+  excluir(id: number) {
+    this._vagasService.removerVaga(id).subscribe(
+      (vaga) => {
+        this.vaga = new Vaga(0, '', '', '', 0);
+      },
+      (err) => {
+        console.log('erro ao excluir');
+      }
+    );
+    window.location.href = '/mural-vagas';
+  }
+}
